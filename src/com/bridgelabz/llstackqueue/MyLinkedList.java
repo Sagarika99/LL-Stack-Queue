a0cc77fecf158d9157136588b7f53bf5f34e05f0
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-08-25 10:59:06 +0530

    Added push method for stack

---
+++ src/com/bridgelabz/llstackqueue/MyLinkedList.java
@@ -0,0 +1,43 @@
+package com.bridgelabz.llstackqueue;
+
+public class MyLinkedList <K> {
+
+	private INode head;
+	private INode tail;
+	
+	public MyLinkedList(){
+		this.head=null;
+		this.tail=null;
+	}
+	
+	public void push(INode newNode) {
+		add(newNode);
+	}
+	
+	public void add(INode newNode) {
+		if (this.head == null) {
+			this.head = newNode;
+		}
+		if (this.tail == null) {
+			this.tail = newNode;
+		} 
+		else {
+			INode tempNode = this.head;
+			this.head = newNode;
+			this.head.setNext(tempNode);
+		}		
+	}
+	
+	public void printStack() {
+		StringBuffer printformat = new StringBuffer("My Nodes: ");
+		INode tempNode = head;
+		while (tempNode != null) {
+			printformat.append(tempNode.getKey());
+			if (tempNode != tail) {
+				printformat.append("-->");
+			}
+			tempNode = tempNode.getNext();
+		}
+		System.out.println(printformat);
+	}
+}
--- src/com/bridgelabz/llstackqueue/StackRunner.java
+++ src/com/bridgelabz/llstackqueue/StackRunner.java
@@ -2,4 +2,17 @@ package com.bridgelabz.llstackqueue;
 
 public class StackRunner {
 
+public static void main(String[] args) {
+	MyNode<Integer> myFirstNode = new MyNode<Integer>(70);
+	MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+	MyNode<Integer> myThirdNode = new MyNode<Integer>(56);
+	
+	MyLinkedList stack = new MyLinkedList();
+	
+	stack.push(myFirstNode);
+	stack.push(mySecondNode);
+	stack.push(myThirdNode);
+	stack.printStack();
+	
+	}
 }
