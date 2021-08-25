40b4a3c069e9da79a3288f9fd083a89df24aec21
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-08-25 14:27:10 +0530

    Enqueue method added for queue

---
+++ src/com/bridgelabz/llstackqueue/MyQueue.java
@@ -0,0 +1,43 @@
+package com.bridgelabz.llstackqueue;
+
+public class MyQueue <K> {
+
+	private INode head;
+	private INode tail;
+	
+	public MyQueue(){
+		this.head=null;
+		this.tail=null;
+	}
+	
+	public void enqueue(INode newNode) {
+		append(newNode);
+	}
+	
+	public void append(INode newNode) {
+		if (this.head == null) {
+			this.head = newNode;
+		}
+		if (this.tail == null) {
+			this.tail = newNode;
+		}
+		else {
+			this.tail.setNext(newNode);
+			this.tail = newNode;
+		}
+	}
+	
+	public void printQueue() {
+		StringBuffer printformat = new StringBuffer("My Nodes: ");
+		INode tempNode = head;
+		while (tempNode != null) {
+			printformat.append(tempNode.getKey());
+			if (tempNode != tail) {
+				printformat.append("-->");
+			}
+			tempNode = tempNode.getNext();
+		}
+		System.out.println(printformat);
+	}
+	
+}
---
+++ src/com/bridgelabz/llstackqueue/QueueRunner.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.llstackqueue;
+
+public class QueueRunner {
+
+	public static void main(String[] args) {
+		MyNode<Integer> myFirstNode = new MyNode<Integer>(70);
+		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+		MyNode<Integer> myThirdNode = new MyNode<Integer>(56);
+		
+		MyQueue queue = new MyQueue();
+		
+		queue.enqueue(myThirdNode);
+		queue.enqueue(mySecondNode);
+		queue.enqueue(myFirstNode);
+		queue.printQueue();
+		
+	}
+}
